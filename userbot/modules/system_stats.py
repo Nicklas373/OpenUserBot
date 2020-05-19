7f47d47f6b55693ee29ae03dfa85bf3d56aac2e4
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-05-19 07:18:38 +0700

    modules: system_stats: misc changes for my greeting :p
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -133,7 +133,7 @@ async def pipcheck(pip):
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("`"
-                     "HANA-CI Userbot is running (˃ᴗ˂) \n\n"
+                     "こんにちは ~ HANA-CI Userbot Here (˃ᴗ˂) ~ \n\n"
                      f"Telethon version: {version.__version__} \n"
                      f"Python: {python_version()} \n"
                      f"User: @Nicklas373 \n"
